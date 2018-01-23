c2406fe4cc94f3fd73732eeb737fdeeae88f56a6
Elizabeth E <embrokoh@gmail.com>
2018-01-23 14:21:04 -0500

    project 3

---
+++ history/pg2/script.js
@@ -0,0 +1,96 @@
+(function () {
+
+  // VARIABLES
+  var timeline = document.querySelector(".timeline ol"),
+      elH = document.querySelectorAll(".timeline li > div"),
+      arrows = document.querySelectorAll(".timeline .arrows .arrow"),
+      arrowPrev = document.querySelector(".timeline .arrows .arrow__prev"),
+      arrowNext = document.querySelector(".timeline .arrows .arrow__next"),
+      firstItem = document.querySelector(".timeline li:first-child"),
+      lastItem = document.querySelector(".timeline li:last-child"),
+      xScrolling = 280,
+      disabledClass = "disabled";
+
+  // START
+  window.addEventListener("load", init);
+
+  function init() {
+    setEqualHeights(elH);
+    animateTl(xScrolling, arrows, timeline);
+  }
+
+  // SET EQUAL HEIGHTS
+  function setEqualHeights(el) {
+    var counter = 0;
+    for (var i = 0; i < el.length; i++) {if (window.CP.shouldStopExecution(1)){break;}
+      var singleHeight = el[i].offsetHeight;
+
+      if (counter < singleHeight) {
+        counter = singleHeight;
+      }
+    }
+window.CP.exitedLoop(1);
+
+
+    for (var _i = 0; _i < el.length; _i++) {if (window.CP.shouldStopExecution(2)){break;}
+      el[_i].style.height = counter + "px";
+    }
+window.CP.exitedLoop(2);
+
+  }
+
+  // CHECK IF AN ELEMENT IS IN VIEWPORT
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+  }
+
+  // SET STATE OF PREV/NEXT ARROWS
+  function setBtnState(el) {
+    var flag = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+
+    if (flag) {
+      el.classList.add(disabledClass);
+    } else {
+      if (el.classList.contains(disabledClass)) {
+        el.classList.remove(disabledClass);
+      }
+      el.disabled = false;
+    }
+  }
+
+  // ANIMATE TIMELINE
+  function animateTl(scrolling, el, tl) {
+    var counter = 0;
+    for (var i = 0; i < el.length; i++) {if (window.CP.shouldStopExecution(3)){break;}
+      el[i].addEventListener("click", function () {
+        if (!arrowPrev.disabled) {
+          arrowPrev.disabled = true;
+        }
+        if (!arrowNext.disabled) {
+          arrowNext.disabled = true;
+        }
+        var sign = this.classList.contains("arrow__prev") ? "" : "-";
+        if (counter === 0) {
+          tl.style.transform = "translateX(-" + scrolling + "px)";
+        } else {
+          var tlStyle = getComputedStyle(tl);
+          // add more browser prefixes if needed here
+          var tlTransform = tlStyle.getPropertyValue("-webkit-transform") || tlStyle.getPropertyValue("transform");
+          var values = parseInt(tlTransform.split(",")[4]) + parseInt("" + sign + scrolling);
+          tl.style.transform = "translateX(" + values + "px)";
+        }
+
+        setTimeout(function () {
+          isElementInViewport(firstItem) ? setBtnState(arrowPrev) : setBtnState(arrowPrev, false);
+          isElementInViewport(lastItem) ? setBtnState(arrowNext) : setBtnState(arrowNext, false);
+        }, 1100);
+
+        counter++;
+      });
+    }
+window.CP.exitedLoop(3);
+
+  }
+})();
---
+++ practice.js
@@ -0,0 +1,2 @@
+for (var number = 0; number <= 12; number = number + 2)
+  console.log(number);
